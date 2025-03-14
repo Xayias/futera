6c8561e373d2310b47abbb7f3a05b076aa6c8087
Xayias <Xayias1@yahoo.com>
2025-03-14 13:57:07 -0500

    Added fontawesome packages

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../assets/futeralogo-white-long.webp'
+import {
+    FaDribbbleSquare,
+    FaFacebookSquare,
+    FaGithubSquare,
+    FaInstagram,
+    FaTwitterSquare,
+} from 'react-icons/fa'
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Analytics from "../components/Analytics";
-import Newsletter from "../components/Newsletter";
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+import Analytics from "../components/Analytics"
+import Newsletter from "../components/Newsletter"
 import Cards from "../components/Cards"
+import Footer from "../components/Footer"
 
 const HomeDetails = () => {
     return (
@@ -13,6 +14,7 @@ const HomeDetails = () => {
             <Analytics />
             <Newsletter />
             <Cards />
+            <Footer />
         </div>
     )
 }
