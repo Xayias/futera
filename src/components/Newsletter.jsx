cf1f3346aa95019ab0cd04c15f83cb9d3c5aa372
Xayias <Xayias1@yahoo.com>
2025-03-14 13:22:18 -0500

    Added Newsletter component to Home

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -10,7 +10,7 @@ const Newsletter = () => {
                 </div>
                 <div className="my-4">
                     <div className="flex flex-col sm:flex-row items-center justify-center w-full">
-                        <input className="p-3 w-full rounded-md text-white border-white" type="email" placeholder="Enter Email" />
+                        <input className="p-3 mx-auto w-full rounded-md text-white border border-white" type="email" placeholder="Enter Email" />
                         <button className="bg-[#1bb8d7] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black sm:ml-4 px-6">Notify Me</button>
                     </div>
                     <p>We care about the protection of your data. Read our <span className='text-[#1bb8d7]'>Privacy Policy</span>.</p>
