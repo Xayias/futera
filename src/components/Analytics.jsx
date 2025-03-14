af7c3756788362757a3047021e31efd9d4882cfa
Xayias <Xayias1@yahoo.com>
2025-03-14 12:54:09 -0500

    Changed text to block on anaytics component

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -8,8 +8,8 @@ const Analytics = () => {
                 <img className="w-[500px] mx-auto my-4" src={DigitalWallet} alt="Digital wallet displaying out of a laptop" />
                 <div className="flex flex-col jusitfy-center">
                     <p className="text-[#1bb8d7] font-bold">DATA ANALYTICS DASHBOARD</p>
-                    <h2 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Manage Data Analytics Centrally</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, sapiente vel tenetur, ab impedit, ex saepe quo officia excepturi illum fugiat esse vero? Molestiae dolorem, vel vitae quisquam veniam accusantium.</p>
+                    <h2 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2 text-black">Manage Data Analytics Centrally</h2>
+                    <p className="text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, sapiente vel tenetur, ab impedit, ex saepe quo officia excepturi illum fugiat esse vero? Molestiae dolorem, vel vitae quisquam veniam accusantium.</p>
                     <button className="bg-[#000] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-[#1bb8d7]">Get Started</button>
                 </div>
             </div>
