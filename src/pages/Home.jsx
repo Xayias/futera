d2ab49622a3d9f90ffc02467fe1074b36e065ced
Xayias <Xayias1@yahoo.com>
2025-03-14 13:12:19 -0500

    Added Newsletter component to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Analytics from "../components/Analytics";
-import Newsletter from "../compontents/Newsletter";
+import Newsletter from "../components/Newsletter";
 
 const HomeDetails = () => {
     return (
