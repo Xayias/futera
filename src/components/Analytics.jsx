36152739a4b978a99b8bdea5ca28a59160fc972b
Xayias <Xayias1@yahoo.com>
2025-03-14 12:47:05 -0500

    Added Analytics component to Home

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import DigitalWallet from '../assets/DigitalWalletLaptop.webp';
+
+const Analytics = () => {
+    return (
+        <div className="w-full bg-white py-16 px-4">
+            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+                <img className="w-[500px] mx-auto my-4" src={DigitalWallet} alt="Digital wallet displaying out of a laptop" />
+                <div className="flex flex-col jusitfy-center">
+                    <p className="text-[#1bb8d7] font-bold">DATA ANALYTICS DASHBOARD</p>
+                    <h2 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Manage Data Analytics Centrally</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, sapiente vel tenetur, ab impedit, ex saepe quo officia excepturi illum fugiat esse vero? Molestiae dolorem, vel vitae quisquam veniam accusantium.</p>
+                    <button className="bg-[#000] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-[#1bb8d7]">Get Started</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Analytics
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Analytics from "../components/Analytics";
 
 const HomeDetails = () => {
     return (
         <div className="home-page">
             <Navbar />
             <Hero />
+            <Analytics />
         </div>
     )
 }
