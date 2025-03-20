67cd7979ee4128eb7ffa928d10398bed185c4aff
Xayias <Xayias1@yahoo.com>
2025-03-20 14:00:47 -0500

    Applied flex-col to div on hero for medium breakpoint

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <div className="relative mx-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center px-4">
                 <p className="text-[#1bb8d7] font-bold p-2">INNOVATING WITH ADVANCED TECHNOLOGIES</p>
                 <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-1">Future Proofing Tomorrow's Connectivity.</h1>
-                <div className="flex justify-center md:flex-col items-center">
+                <div className="flex justify-center flex-col md:flex-row items-center">
                     <p className="md:text-4xl sm:text-3xl text-xl font-bold py-4">Driving Innovation in</p>
                     <ReactTyped className="md:text-4xl sm:text-3xl text-xl font-bold md:pl-4 pl-2" strings={["Networking", "Machine Learning", "Data Security"]} typeSpeed={120} backSpeed={140} loop />
                 </div>
