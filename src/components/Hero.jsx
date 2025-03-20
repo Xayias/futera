4e3c3360f8044979b526d67e04f87aa426b146f1
Xayias <Xayias1@yahoo.com>
2025-03-20 14:02:45 -0500

    Fixed padding on typed section for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                 <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-1">Future Proofing Tomorrow's Connectivity.</h1>
                 <div className="flex justify-center flex-col md:flex-row items-center">
                     <p className="md:text-4xl sm:text-3xl text-xl font-bold py-4">Driving Innovation in</p>
-                    <ReactTyped className="md:text-4xl sm:text-3xl text-xl font-bold md:pl-4 pl-2" strings={["Networking", "Machine Learning", "Data Security"]} typeSpeed={120} backSpeed={140} loop />
+                    <ReactTyped className="md:text-4xl sm:text-3xl text-xl font-bold md:pl-4 pl-2 pb-4 md:pb-0" strings={["Networking", "Machine Learning", "Data Security"]} typeSpeed={120} backSpeed={140} loop />
                 </div>
                 <p className="md:text-2xl text-xl font-bold text-gray-500">Secure, scalable infrastructure for data centers and AI-driven insights.</p>
                 <button className="bg-[#1bb8d7] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black;">Explore Solutions</button>
