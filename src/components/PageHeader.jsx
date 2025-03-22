c78b91c358f518e9410b6d95554775e5e08c0c75
Xayias <Xayias1@yahoo.com>
2025-03-21 22:51:52 -0500

    Adding basic page header component

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageHeader = ({ title, subtitle, backgroundImage }) => {
     return (
-        <div className="text-center my-8 bg-[image:var(--image-url)] bg-cover bg-center text-white py-20" 
+        <div className="text-center my-8 bg-cover bg-center text-white py-20" 
         style={{backgroundImage: 'url(${backgroundImage})'}}>
             <h1 className="text-4xl font-bold">{title}</h1>
             <p className="text-lg mt-2">{subtitle}</p>
