3535aeb2ce27cebf35f8512570247d14d209805c
Xayias <Xayias1@yahoo.com>
2025-03-22 14:40:39 -0500

    Created content for highlight on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer"
 import PageHeader from "../components/PageHeader"
 import headerImage from "../assets/aboutheaderbg.webp"
 import Highlight from "../components/Highlight"
-import highlightImage from "../assets/react.svg"
+import highlightImage from "../assets/abouthighlight.webp"
 
 const AboutDetails = () => {
     return (
@@ -17,11 +17,11 @@ const AboutDetails = () => {
                 backgroundImage={headerImage}
             />
             <Highlight 
-                highlightTitle=""
-                highlightSubtitle=""
+                highlightTitle="Bringing Innovation for the Future"
+                highlightSubtitle="PEOPLE THAT CONNECTS US ALL"
                 highlightImage={highlightImage}
-                highlightParagraph=""
-                highlightCTA=""
+                highlightParagraph="We are a team dedicated to improving the wider internet and tech. Learn how we shape our enviornment to help and educate those dedicated to serving those through technology."
+                highlightCTA="Read Our Story"
             />
             <Footer />
         </div>
