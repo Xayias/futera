dbd79da8582363fa3d5f1a3d0fa5196c6b76a574
Xayias <Xayias1@yahoo.com>
2025-03-24 10:31:56 -0500

    Fixed React Import on Contact Form Component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ContactForm = () => {
     const [form, setForm] = useState({ name: '', email: '', subject: '', message: ''});
